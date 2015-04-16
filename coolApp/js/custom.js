035669b436cd5ea23ee39d0e06ed7ca776ce6d9e
lit-john <john.hannafin@lit.ie>
2015-04-16 15:47:00 +0000

    Added allNames functionality

--- coolApp/js/custom.js
+++ coolApp/js/custom.js
@@ -8,11 +8,50 @@ $(document).ready(function() {
       method: "GET",
       url: "//obscure-tundra-3047.herokuapp.com/api/yourName"
     }).done(function(response) {
+      
       console.log(response.name);
+      
+      $('div#your-name').html("<p>"+response.name+"</p>");
+      
     }).fail(function(mgs) {
       
     });
     
   });
   
+  
+  
+  
+  $('button#button-my-name').click(function() {
+    console.log("button-my-name clicked");
+    
+    var theName = $('input#myName').val();
+    
+    $.ajax({
+      method: "POST",
+      data : {myName: theName},
+      url: "//obscure-tundra-3047.herokuapp.com/api/myName"
+    }).done(function(response) {
+      console.log(response.allNames);
+      
+      var theNamesHtml = "<ul id=\"allNames\">";
+      
+      for (var i=0; i < response.allNames.length; i++) {
+        theNamesHtml += "<li>" + response.allNames[i] + "</li>";
+      }
+      
+      theNamesHtml += "</ul>"
+      
+      $('div#my-name-response').html(theNamesHtml);
+    }).fail(function(msg) {
+      console.log(msg);
+    });
+  });
+  
 });
+
+
+
+
+
+
