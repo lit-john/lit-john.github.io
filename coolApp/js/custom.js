29b1fe2e0b02199f1c2221596588f699afbed991
lit-john <john.hannafin@lit.ie>
2015-04-16 16:09:03 +0000

    Added timeout to get names

--- coolApp/js/custom.js
+++ coolApp/js/custom.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
   
+  function getAllNames() {
+    $.ajax({
+      method: "GET",
+      url: "//obscure-tundra-3047.herokuapp.com/api/allNames"
+    }).done(function(response) {
+      console.log(response.allNames);
+      
+      var theNamesHtml = "<ul id=\"allNames\">";
+      
+      for (var i=0; i < response.allNames.length; i++) {
+        theNamesHtml += "<li>" + response.allNames[i] + "</li>";
+      }
+      
+      theNamesHtml += "</ul>"
+      
+      $('div#my-name-response').html(theNamesHtml);
+      
+      setTimeout(getAllNames, 1000);
+      
+    }).fail(function(msg) {
+      console.log(msg);
+    });
+    
+    
+  }
+  
+  getAllNames();
+  
+  
+  
+  
   $('button#your-name-button').click(function(){
     
     console.log("your-name-button clicked");
@@ -26,6 +57,7 @@ $(document).ready(function() {
     console.log("button-my-name clicked");
     
     var theName = $('input#myName').val();
+    $('input#myName').val("");
     
     $.ajax({
       method: "POST",
