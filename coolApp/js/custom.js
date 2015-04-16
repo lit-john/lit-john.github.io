4303590379230bb1311398c0d42457c546338d35
lit-john <john.hannafin@lit.ie>
2015-04-16 14:19:58 +0000

    Fixed ajax url

--- coolApp/js/custom.js
+++ coolApp/js/custom.js
@@ -4,6 +4,15 @@ $(document).ready(function() {
     
     console.log("your-name-button clicked");
     
+    $.ajax({
+      method: "GET",
+      url: "//obscure-tundra-3047.herokuapp.com/api/yourName"
+    }).done(function(response) {
+      console.log(response.name);
+    }).fail(function(mgs) {
+      
+    });
+    
   });
   
 });
